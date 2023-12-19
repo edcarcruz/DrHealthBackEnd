dcf0177d13e68e118bcdb03a123e4ecc4b4ab15e
mherrera491 <mattherrera1@gmail.com>
2023-12-18 19:42:49 -0800

    added function names

--- controllers/illnessCtrls.js
+++ controllers/illnessCtrls.js
@@ -1,11 +1,21 @@
 const db = require("../models");
 
 // Get Illness
+const getIllness
 
 // Create Illness
+const createIllness
 
 // Update Illness 
+const updateIllness
 
 // Delete Illness
+const deleteIllness
 
 // export functions
+module.exports = {
+    getIllness,
+    createIllness,
+    deleteIllness,
+    updateIllness
+}
